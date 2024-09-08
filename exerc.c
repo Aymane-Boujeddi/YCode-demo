42a831e1f4884a51f024c5ded372b16d3aeb862d
Aymane Boujeddi <aymane655@gmail.com>
2024-09-08 18:44:28 +0100

    first commit

---
+++ exerc.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+int main()
+{
+int i , j;
+for(i=0;i<=10;i++){
+            printf("%d\t",i);
+}
+     for(j=0;j<i;j++){
+
+            printf("%d\n\n %d\t",j,i*j);
+
+
+     }
+
+
+
+
+
+
+
+
+
+
+   return 0;
+}
